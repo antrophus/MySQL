a5399d6d08bd3d5fb25159cdccaeea6897a91221
함민규 <antrophus@yahoo.com>
2024-07-21 19:07:26 +0900

    Update practice.sql

--- Practice/practice.sql
+++ Practice/practice.sql
@@ -13,7 +13,7 @@ select  concat(first_name,' ',last_name) 이름
         ,hire_date 입사일
 from employees
 order by hire_date asc, first_name asc
-;
+	;
 
 /*
 문제2.
@@ -21,20 +21,16 @@ order by hire_date asc, first_name asc
 하세요.
 */
 select  job_title 업무이름
-		,max_salary 최고월급
-        from jobs
-		order by max_salary desc;
-/*
-문제2 - 1.
-업무(jobs)별로 업무이름(job_title)과 최고월급(max_salary)을 월급의 내림차순(DESC)로 정렬
-하세요.
-*/ 
-select 
-		job_title 업무이름
-		,max_salary 최고월급
+	,max_salary 최고월급
+from jobs
+order by max_salary desc
+	;
+------------------------------------------------------------------------------------
+select 	job_title 업무이름
+	,max_salary 최고월급
         ,salary 월급
-		from jobs, employees
-		order by salary desc
+from jobs, employees
+order by salary desc
         ;       
         
 /*
@@ -43,25 +39,25 @@ select
 아이디, 커미션 비율, 월급을 월급이 많은사람부터 출력하세요.
 */
 select  first_name 이름
-		,manager_id '매니저 아이디'
+	,manager_id '매니저 아이디'
         ,commission_pct '커미션 비율'
         ,salary 월급
-		from employees
-        where salary > 3000 
-        and manager_id is not null
-        and commission_pct is null 
-        order by salary desc
-		;
+from employees
+where salary > 3000 
+and manager_id is not null
+and commission_pct is null 
+order by salary desc
+	;
 /*
 문제4.
 최고월급(max_salary)이 10000 이상인 업무의 이름(job_title)과 최고월급(max_salary)을 최
 고월급의(max_salary) 내림차순(DESC)로 정렬하여 출력하세요.
 */
 select  job_title 업무이름
-		,max_salary 최고월급
-		from jobs
-    where salary >= 10000
-        order by max_salary desc
+	,max_salary 최고월급
+from jobs
+where salary >= 10000
+order by max_salary desc
         ;
 
 /*
@@ -70,13 +66,13 @@ select  job_title 업무이름
 (내림차순) 출력하세오. 단 커미션퍼센트 가 null 이면 0 으로 나타내시오
 */
 select  first_name 이름
-		,salary 월급
+	,salary 월급
         ,ifnull(commission_pct, 0) 수당비율
-        from employees
-		where salary >= 10000
-		and salary < 14000
-		order by salary desc
-		;
+from employees
+where salary >= 10000
+and salary < 14000
+order by salary desc
+	;
 /*
 문제6.
 부서번호가 10, 90, 100 인 직원의 이름, 월급, 입사일, 부서번호를 나타내시오
@@ -84,22 +80,31 @@ select  first_name 이름
 입사일은 1977-12 와 같이 표시하시오
 */
 select  first_name 이름
-		,salary 월급
+	,salary 월급
         ,date_format(hire_date, '%Y-%m') 입사일
         ,department_id 부서번호
-		from employees
-        where department_id = 10
-        or department_id = 90
-        or department_id = 100
-		;
+from employees
+where department_id = 10
+or department_id = 90
+or department_id = 100
+	;
+------------------------------------------------------------------------
+select  first_name 이름
+	,salary 월급
+        ,date_format( hire_date, '%Y-%m') 입사일
+        ,department_id 부서번호
+from employees
+where department_id in(10,90,100)
+;
+
 /*
 문제7.
 이름(first_name)에 S 또는 s 가 들어가는 직원의 이름, 월급을 나타내시오
 */
 select  first_name 
-		,salary 
-		from employees
-        where first_name like '%s%'
+	,salary 
+from employees
+where first_name like '%s%'
         ;
 		
 /*
@@ -110,6 +115,12 @@ select *
 from departments
 order by length(department_name) desc
 ;
+-------------------------------------------------------------------
+select 	department_name
+		,length(department_name)
+from departments
+order by length(department_name) desc
+;
 
 /*
 문제9.
@@ -117,19 +128,28 @@ order by length(department_name) desc
 올림차순(ASC)으로 정렬해 보세오.
 */
 select  upper(country_name)
-		
-		from countries
-		order by country_name asc
-		;
+from 	countries
+order by country_name asc
+	;
 /*
 문제10.
 입사일이 03/12/31 일 이전 입사한 직원의 이름, 월급, 전화 번호, 입사일을 출력하세요
 전화번호는 545-343-3433 과 같은 형태로 출력하시오.
 */
 select  first_name 이름
-		,salary 월급
+	,salary 월급
         ,replace(phone_number, '.', '-') 전화번호
         ,hire_date 입사일
 from employees
 where hire_date <= '2003/12/31'
-		;
+	;
+
+--------------------------------------------------------------------
+select  first_name 이름
+	,salary 월급
+        ,replace(phone_number, '.', ' - ')  '전화 번호'
+        ,hire_date 입사일
+from employees
+where hire_date <= '2003-12-31'
+order by hire_date desc
+;
